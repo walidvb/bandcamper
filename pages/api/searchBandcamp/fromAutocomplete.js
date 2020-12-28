90043899b1f20cc90da27a34f2c860c05e6a7e5f
Walid <hello@walidvb.com>
2020-12-28 20:28:27 -0300

    fixes search

--- pages/api/searchBandcamp/fromAutocomplete.js
+++ pages/api/searchBandcamp/fromAutocomplete.js
@@ -5,7 +5,7 @@ const BRANDCAMP_SEARCH_URL = "https://bandcamp.com/api/nusearch/2/autocomplete?q
 const searchApi = async ({ artist, name }) => {
   const queryURL = `${BRANDCAMP_SEARCH_URL}${encodeURIComponent(`${artist} ${name}`)}`
   const results = await Axios.get(queryURL)
-  return results.data.auto.results
+  return results.data.results
 }
 
 export default searchApi
