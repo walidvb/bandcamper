564635c4ef03492117351c3808e26b1b3570d063
Walid <hello@walidvb.com>
2020-12-28 22:07:04 -0300

    adds player progress

--- components/BandcampURLInput/BandcampPlayer.js
+++ components/BandcampURLInput/BandcampPlayer.js
@@ -32,8 +32,7 @@ function BandcampPlayer(props) {
   return (
       <iframe
         className={'miniplayer'}
-        // src={`https://bandcamp.com/EmbeddedPlayer/album=${album_id || id }/size=small/bgcol=E2E8F0/linkcol=2c5282/transparent=true/`}
-      src={`https://bandcamp.com/EmbeddedPlayer/album=3882342693/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=3899568748/transparent=true/`}
+      src={`https://bandcamp.com/EmbeddedPlayer/album=${album_id || id }/size=large/bgcol=E2E8F0/linkcol=2c5282/tracklist=false/artwork=small/transparent=true/`}
         seamless
       />
   );
